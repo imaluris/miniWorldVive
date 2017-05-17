220603189294dcbfd2bad409ad8821c24d7032b0
Andrea <andrea.93@hotmail.it>
2017-05-17 21:52:39 +0200

    app.js modified

--- src/app.js
+++ src/app.js
@@ -12,20 +12,3 @@ extras.controls.registerAll();
 
 // Register everything.
 extras.registerAll();
-
-var el = sceneEl.querySelector('#susanne');
-el.body.applyImpulse(
-  /* impulse */        new CANNON.Vec3(0, 1, -1),
-  /* world position */ new CANNON.Vec3().copy(el.getComputedAttribute('position'))
-);
-
-var playerEl = document.querySelector('[camera]');
-playerEl.addEventListener('collide', function (e) {
-console.log('Player has collided with body #' + e.detail.body.id);
-
-e.detail.target.el;  // Original entity (playerEl).
-e.detail.body.el;    // Other entity, which playerEl touched.
-e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
-e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
-});
-
